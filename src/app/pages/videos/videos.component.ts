ce304916a8de03005493de50d4baa46ad18ef3f7
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2025-03-16 22:49:08 +0500

    filter video

--- src/app/pages/videos/videos.component.ts
+++ src/app/pages/videos/videos.component.ts
@@ -9,32 +9,33 @@ import { title } from 'node:process';
 })
 export class VideosComponent implements OnInit {
   videos = [
-    { id: '8nQQM7MZXuU', title: 'EntityFrameWorCore 2-qism: Controller, Migration, API vahokazo' },
-    { id: 'Gx9m2YCxFyA', title: 'EntityFrameWorkCore: ConectionString sozlashgacha...' },
-    { id: 'tsuLo29faqc', title: 'LINQ: Where, Select, OrderBy, OrderByDecending...' },
-    { id: 'N28rTpoJqUI', title: 'C++: C++ tilidagi ma\'limot turlari va o\'zgaruvchilar' },
-    { id: 'UIubOOngvjM', title: 'Twillio: SMS jo\'natish tizimi Twillio configuratsiyasi orqali' },
-    { id: 'UKr-la5-B3o', title: 'ADO.NET: Databazaga malimot yuklab, malimot o\'qish' },
-    { id: 'JbdoXk4_cUw', title: 'PGAdmin: PostgreSQLdagi kuchliroq darajadagi metodlar va amaliyot' },
-    { id: 'c-DOKHY7FoU', title: 'PGAdmin: Databaza nima va postgreSQLda CRUD operatsiyasi' },
-    { id: 'fEsDHGRH6Fc', title: 'Flowbite: Code \'yozmasdan\' responsive va zamonaviy sayt yasash' },
-    { id: 'M-Vb3pqk3qg', title: 'Inline Buttons: C# Telegram botda inline tugmachalar va kichik proyekt' },
-    { id: '4RdhKoCijIE', title: 'Keyboard Buttons: C# Telegram botda klaviatura tugmachalari' },
-    { id: '-e4cRXphdjc', title: 'Stack VS Queue: C# dagi FIFO va LIFO nazariyalari' },
-    { id: 'tVacciUhTzY', title: 'TG Bot OOP: classlar orqali telegram bot yasash | qulayroq usul' },
-    { id: '-4ywLoiHybU', title: 'C# Dictionary: C# tilida dictionary\'dan foydalaning!!!' },
-    { id: 'ZSDX-gddjtw', title: 'Services & Interface: C#da interface va service nima???' },
-    { id: 'YoOVaRWpP8c', title: 'Class Integration: Boshqa fayldagi classni program.cs da chqarib, foydalanish' },
-    { id: 'pienoKPcWWA', title: 'Kutubxona loyihasi 2-qism: classdagi obyektlarga foreach orqali murojaat qilish' },
-    { id: 'eFtZRT05zMI', title: 'Kutubxona loyihasi 1-qism: C#da get & set nima?' },
-    { id: '7-ocA8vleU0', title: 'CCC: C# Class Constructori | Class haqida batafsil' },
-    { id: 'HLEafCWAK2k', title: 'Params & Exception: Chegarasiz parameter qabul qilish & Xatosizlantirish ' },
-    { id: '6r1zzgJuwWg', title: 'C# Methods: Void va Returnli funksiyalar farqlari batafsil' },
-    { id: 'xz_sPVhd-zI', title: 'Random kutubxonasi: C#da tosh, qaychi, qog\'oz o\'yini random orqali' },
-    { id: 'tSGsBhsdIBU', title: 'Bubble Sort: Bubble sort qanday algoritm asosida ishlashini c#da o\'rganing' },
-    { id: 'UaAu8NhUiFs', title: 'C# List, While & For: List elementlarni while va for looplari orqali aylantirish' },
-    { id: 'OLz9c0DE9gE', title: '42 Mimic: Htm & Css dan foydalanib 42.uz saytini 0dan yozish'},
-    { id: 'aoaHX-SJynY', title: 'Sakkiz: Kanalga post qoyadigan admin bolish uchun royhatdan otish'}
+    { id: 'L8Egk_GQHZc', title: 'C++da qiymat o\'zgartirish: faqat 1 soatgacha dars', type: 'C++' },
+    { id: '8nQQM7MZXuU', title: 'EntityFrameWorCore 2-qism: Controller, Migration, API vahokazo', type: 'C#' },
+    { id: 'Gx9m2YCxFyA', title: 'EntityFrameWorkCore: ConectionString sozlashgacha...', type: 'C#' },
+    { id: 'tsuLo29faqc', title: 'LINQ: Where, Select, OrderBy, OrderByDecending...', type: 'C#' },
+    { id: 'N28rTpoJqUI', title: 'C++: C++ tilidagi ma\'limot turlari va o\'zgaruvchilar', type: 'C++' },
+    { id: 'UIubOOngvjM', title: 'Twillio: SMS jo\'natish tizimi Twillio configuratsiyasi orqali', type: 'C#' },
+    { id: 'UKr-la5-B3o', title: 'ADO.NET: Databazaga malimot yuklab, malimot o\'qish', type: 'C#' },
+    { id: 'JbdoXk4_cUw', title: 'PGAdmin: PostgreSQLdagi kuchliroq darajadagi metodlar va amaliyot', type: 'C#' },
+    { id: 'c-DOKHY7FoU', title: 'PGAdmin: Databaza nima va postgreSQLda CRUD operatsiyasi', type: 'C#' },
+    { id: 'fEsDHGRH6Fc', title: 'Flowbite: Code \'yozmasdan\' responsive va zamonaviy sayt yasash', type: 'Other' },
+    { id: 'M-Vb3pqk3qg', title: 'Inline Buttons: C# Telegram botda inline tugmachalar va kichik proyekt', type: 'C#' },
+    { id: '4RdhKoCijIE', title: 'Keyboard Buttons: C# Telegram botda klaviatura tugmachalari', type: 'C#' },
+    { id: '-e4cRXphdjc', title: 'Stack VS Queue: C# dagi FIFO va LIFO nazariyalari', type: 'C#' },
+    { id: 'tVacciUhTzY', title: 'TG Bot OOP: classlar orqali telegram bot yasash | qulayroq usul', type: 'C#' },
+    { id: '-4ywLoiHybU', title: 'C# Dictionary: C# tilida dictionary\'dan foydalaning!!!', type: 'C#' },
+    { id: 'ZSDX-gddjtw', title: 'Services & Interface: C#da interface va service nima???', type: 'C#' },
+    { id: 'YoOVaRWpP8c', title: 'Class Integration: Boshqa fayldagi classni program.cs da chqarib, foydalanish', type: 'C#' },
+    { id: 'pienoKPcWWA', title: 'Kutubxona loyihasi 2-qism: classdagi obyektlarga foreach orqali murojaat qilish', type: 'C#' },
+    { id: 'eFtZRT05zMI', title: 'Kutubxona loyihasi 1-qism: C#da get & set nima?', type: 'C#' },
+    { id: '7-ocA8vleU0', title: 'CCC: C# Class Constructori | Class haqida batafsil', type: 'C#' },
+    { id: 'HLEafCWAK2k', title: 'Params & Exception: Chegarasiz parameter qabul qilish & Xatosizlantirish', type: 'C#' },
+    { id: '6r1zzgJuwWg', title: 'C# Methods: Void va Returnli funksiyalar farqlari batafsil', type: 'C#' },
+    { id: 'xz_sPVhd-zI', title: 'Random kutubxonasi: C#da tosh, qaychi, qog\'oz o\'yini random orqali', type: 'C#' },
+    { id: 'tSGsBhsdIBU', title: 'Bubble Sort: Bubble sort qanday algoritm asosida ishlashini c#da o\'rganing', type: 'C#' },
+    { id: 'UaAu8NhUiFs', title: 'C# List, While & For: List elementlarni while va for looplari orqali aylantirish', type: 'C#' },
+    { id: 'OLz9c0DE9gE', title: '42 Mimic: Htm & Css dan foydalanib 42.uz saytini 0dan yozish', type: 'Other' },
+    { id: 'aoaHX-SJynY', title: 'Sakkiz: Kanalga post qoyadigan admin bolish uchun royhatdan otish', type: 'Other' }
   ];
 
   filteredVideos = this.videos;
@@ -45,6 +46,15 @@ export class VideosComponent implements OnInit {
   ngOnInit(): void {
     const savedTheme = localStorage.getItem('theme');
     this.isLightTheme = savedTheme === 'light';
+    this.filterVideos('All'); 
+  }
+
+  filterVideos(filter: string): void {
+    if (filter === 'All') {
+      this.filteredVideos = this.videos;
+    } else {
+      this.filteredVideos = this.videos.filter(video => video.type === filter); 
+    }
   }
 
   searchVideos(event: Event): void {
